146cab944b20223f16f4423cfd88fddd04ea3976
anasboumziza <anasboumziza@gmail.com>
2024-02-01 22:15:10 +0100

    adding pion final version

--- Pion.py
+++ Pion.py
@@ -75,4 +75,102 @@ class Pion:
             return self.manger(pion_adverse, damier)
         else : return False
 
-  
+    def manger(self, pion, damier) :
+        print(self, pion)
+        if self.joueur != pion.joueur :
+            self.joueur.ajouterAuCimetiere(pion)
+            pion.joueur.perdrePion(pion)
+            if self.enchainement(damier) : return None
+            return True
+        return False
+
+    # On vérifie si le joueur peut rejouer si oui en renvoi True et le tour ne sera pas passé
+    def enchainement(self, damier) :
+        if (self.x + 2 or self.y + 2 or self.x - 2 or self.y - 2) > len(damier) - 2 : return False
+        if (self.x + 2 or self.y + 2 or self.x - 2 or self.y - 2) < 1 : return False
+
+        pion_environnants = [damier[self.x + 1][self.y + 1], damier[self.x - 1][self.y - 1], damier[self.x - 1][self.y + 1], damier[self.x + 1][self.y - 1]]
+        cases_libres = [damier[self.x + 2][self.y + 2], damier[self.x - 2][self.y - 2], damier[self.x - 2][self.y + 2], damier[self.x + 2][self.y - 2]]
+
+        for i in range(len(pion_environnants)) :
+            if isinstance(pion_environnants[i], (Dame, Pion)) and not isinstance(cases_libres[i], (Dame, Pion)) :
+                if pion_environnants[i].joueur.id != self.joueur.id :
+                    return True
+        return False
+
+    # On transforme le pion courant en dame
+    def promotion(self) :
+        dame = Dame(self.id, self.x, self.y, self.joueur)
+        self.joueur.perdrePion(self)
+        self.joueur.pions.append(dame)
+        return dame
+
+    def hors_plateau(self, dpl_x, dpl_y, coef_x, coef_y, damier) :
+        case_x = self.x + dpl_x
+        case_y = self.y + dpl_y
+
+        if abs(dpl_x) == 1 :
+            if (case_x or case_y) > len(damier) - 1 : return True
+            if (case_x or case_y) < 0 : return True
+        else :
+            if (case_x + coef_x or case_y + coef_y) > len(damier) - 1 : return True
+            if (case_x + coef_x or case_y + coef_y) < 0 : return True
+        return False
+
+    def __eq__(self, other) :
+        if self.x == other.x and self.y == other.y and self.couleur == other.couleur and self.joueur == other.joueur :
+            return True
+
+
+class Dame(Pion) :
+
+    DIST_MAX = 9
+
+    def __init__(self, id, x, y, joueur) :
+        Pion.__init__(self, id, x, y, joueur)
+        self.img = "./assets/crown.png"
+
+    # La Dame peut toujours se déplacer en arrière
+    def deplacement_arriere(self, dpl_x, dpl_y, coef_x, coef_y, damier) :
+        case_x = self.x + dpl_x
+        case_y = self.y + dpl_y
+        if (case_x + coef_x or case_y + coef_y) < len(damier) and (case_x + coef_x or case_y + coef_y) >= 0:
+            return True
+
+    def terminer_deplacement(self, dpl_x, dpl_y, coef_x, coef_y, damier) :
+        # On vérifie si on passe par dessus un pion, si oui on le mange - ok
+        nbre_pions = 0
+        index = 0
+        for i in range(1, abs(dpl_y)) :
+            print(nbre_pions)
+            if isinstance(damier[self.x + dpl_x + (coef_x * i)][self.y + dpl_y + (coef_y * i)], (Pion, Dame)) and nbre_pions < 1:
+                nbre_pions += 1
+            index = i
+        if nbre_pions == 1 : 
+            print("Position du pion mangé :", self.x + dpl_x + coef_x, self.y + dpl_y + coef_y)
+            pion_adverse = damier[self.x + dpl_x + (coef_x * index)][self.y + dpl_y + (coef_y * index)]
+            self.x += dpl_x
+            self.y += dpl_y
+            return self.manger(pion_adverse, damier)
+        elif nbre_pions == 0 : 
+            self.x += dpl_x
+            self.y += dpl_y
+            return True
+        else : return False
+
+    # On vérifie les diagonales, si un enchainement est possible le tour n'est pas passé
+    def enchainement(self, damier) :
+        for i in range(1, self.DIST_MAX) :
+            if (self.x + i + 1 or self.y + i + 1 or self.x - i - 1 or self.y - i - 1) > len(damier) - 1 : return False
+            if (self.x + i + 1 or self.y + i + 1 or self.x - i - 1 or self.y - i - 1) < 0 : return False
+
+            pion_environnants = [damier[self.x + i][self.y + i], damier[self.x - i][self.y - i], damier[self.x - i][self.y + i], damier[self.x + i][self.y - i]]
+            cases_libres = [damier[self.x + i + 1][self.y + i + 1], damier[self.x - i][self.y - i - 1], damier[self.x - i][self.y + i + 1], damier[self.x + i + 1][self.y - i - 1]]
+        
+            for i in range(len(pion_environnants)) :
+                if isinstance(pion_environnants[i], (Dame, Pion)) and not isinstance(cases_libres[i], (Dame, Pion)) :
+                    if pion_environnants[i].joueur.id != self.joueur.id :
+                        return True
+        return False
+
+        
